cf4a237c57e1411688580f9b8fa74944a82eb317
Alexandre Point <apoint@fleex.tv>
2014-12-02 12:07:20 +0100

    Added statistics method in Sample to compare variations of the algo

--- Samples/Program.cs
+++ Samples/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using InvalidEmailDetector.src;
+using SharpEntropy;
 
 namespace Samples
 {
@@ -49,6 +51,55 @@ namespace Samples
 
             Console.WriteLine("----");
             Console.ReadLine();
+        }
+
+        static void Statistics(GisModel model, string testFilePath)
+        {
+            var results = new List<Tuple<string, bool, double>>();
+
+            // create detector from model
+            var invalidEmailDetector = new MaximumEntropyInvalidEmailDetector(model);
+
+            // read all test (email + invalid flag) lines
+            var allLines = File.ReadAllLines(testFilePath);
+            foreach (var line in allLines)
+            {
+                var parts = line.Split('\t');
+                var email = parts.First();
+                var isInvalid = parts.Last() == "1";
+                
+                // store the result of the detection as well as the actual validity of the email
+                var invalidProbability = invalidEmailDetector.GetInvalidProbability(email);
+                results.Add(new Tuple<string, bool, double>(email, isInvalid, invalidProbability));
+            }
+
+            // pretend to send the emails, and stop when we sent to many emails that bounced
+            var nbOfEmailsSent = 0;
+            var nbOfEmailsSentWhichWouldBounce = 0;
+            var maxNbOfBouncePerDay = 25;
+            foreach (var result in results.OrderBy(tup => tup.Item3))
+            {
+                if (nbOfEmailsSentWhichWouldBounce < maxNbOfBouncePerDay)
+                {
+                    nbOfEmailsSent++;
+                    if (result.Item2) { nbOfEmailsSentWhichWouldBounce++; }
+                }
+                Console.WriteLine("{0} ({1})", result.Item1, result.Item2 ? "INVALID": "OK");
+            }
+
+            Console.WriteLine("Email that could have been sent: {0}", nbOfEmailsSent);
+            Console.WriteLine("========");
+
+            // Quick summary of the detection
+            var probaOfInvalidity = 0.2497;
+            var nbOfSamples = results.Count;
+            Console.WriteLine("Nb of samples: {0}", nbOfSamples);
+            var nbOfCorrectResults = results.Count(tup => tup.Item2 == tup.Item3 > probaOfInvalidity);
+            Console.WriteLine("Nb of correct results: {0}", nbOfCorrectResults);
+            var nbOfNotDetected = results.Count(tup => tup.Item2 && !(tup.Item3 > probaOfInvalidity));
+            Console.WriteLine("Nb of not detected: {0}", nbOfNotDetected);
+            var nbOfFalsePositive = results.Count(tup => !tup.Item2 && (tup.Item3 > probaOfInvalidity));
+            Console.WriteLine("Nb of false positive: {0}", nbOfFalsePositive);
         }
     }
 }
